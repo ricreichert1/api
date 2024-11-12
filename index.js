b473af5e23880eca1351e05142f5f3d3a8962557
ricreichert1 <68476137+ricreichert1@users.noreply.github.com>
2024-11-12 16:08:51 -0300

    melhorias+bugs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const pool = new Pool({
 
 app.get('/rick/list', async (req, res) => {
     try {
-        const result = await pool.query('SELECT nome, idade, altura, peso FROM suneca')
+        const result = await pool.query('SELECT nome, idade, altura, peso, cpf, dataRegistro FROM suneca')
         res.json(result.rows)
     } catch (error) {
         console.error('Erro ao listar registros:', error)
@@ -30,18 +30,22 @@ app.get('/rick/list', async (req, res) => {
     }
 })
 
+
 app.post('/rick/add', async (req, res) => {
     console.log('Dados recebidos:', req.body)
-    const { nomeInput, idadeInput, alturaInput, pesoInput } = req.body
+    const { nomeInput, idadeInput, alturaInput, pesoInput, cpfInput, dataRegistroInput } = req.body
+
     try {
+        const alturaTratada = parseFloat((alturaInput || '').replace(',', '.'))
+
         const result = await pool.query(
-            'INSERT INTO suneca (nome, idade, altura, peso) VALUES ($1, $2, $3, $4)',
-            [nomeInput, idadeInput, alturaInput, pesoInput ]
-        );
+            'INSERT INTO suneca (nome, idade, altura, peso, cpf, dataRegistro ) VALUES ($1, $2, $3, $4, $5, $6)',
+            [nomeInput, idadeInput, alturaTratada, pesoInput, cpfInput, dataRegistroInput]
+        )
         res.status(201).json({ message: 'Mocorongo adicionado com sucesso!' })
     } catch (error) {
         console.error('Erro ao adicionar mocorongo:', error)
-        res.status(500).json({ message: 'Erro ao adicionar mocorongo.' })
+        res.status(500).json({ message: 'Erro ao adicionar Mocorongo.' })
     }
 })
 
