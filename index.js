15933ecf2e19d61324fdae26fe6ebf7e11f18cd2
ricreichert1 <ricreichert99@gmail.com>
2024-11-14 11:45:47 -0300

    test

--- index.js
+++ index.js
@@ -15,33 +15,33 @@ app.use(express.json())
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'teste1',
+    database: 'teste',
     password: '2772',
     port: 5432,
 })
 
 app.get('/rick/list', async (req, res) => {
     try {
-        const result = await pool.query('SELECT nome, idade, altura, peso, cpf, dataRegistro FROM suneca')
+        const result = await pool.query('SELECT nome, idade, altura, peso, cpf, dataregistro FROM suneca')
         res.json(result.rows)
     } catch (error) {
         console.error('Erro ao listar registros:', error)
         res.status(500).json({ message: 'Erro ao listar registros.' })
     }
-})
+});
 
 
 app.post('/rick/add', async (req, res) => {
-    console.log('Dados recebidos:', req.body)
-    const { nomeInput, idadeInput, alturaInput, pesoInput, cpfInput, dataRegistroInput } = req.body
+    console.log('Dados recebidos:', req.body);
+    const { nomeInput, idadeInput, alturaInput, pesoInput, cpfInput } = req.body
 
     try {
         const alturaTratada = parseFloat((alturaInput || '').replace(',', '.'))
 
-        const result = await pool.query(
-            'INSERT INTO suneca (nome, idade, altura, peso, cpf, dataRegistro ) VALUES ($1, $2, $3, $4, $5, $6)',
-            [nomeInput, idadeInput, alturaTratada, pesoInput, cpfInput, dataRegistroInput]
-        )
+        await pool.query(
+            'INSERT INTO suneca (nome, idade, altura, peso, cpf, dataregistro) VALUES ($1, $2, $3, $4, $5, NOW())',
+            [nomeInput, idadeInput, alturaTratada, pesoInput, cpfInput]
+        );
         res.status(201).json({ message: 'Mocorongo adicionado com sucesso!' })
     } catch (error) {
         console.error('Erro ao adicionar mocorongo:', error)
