99f26d64c65b5b7a83436e2960a8662669eedca7
ricreichert1 <ricreichert99@gmail.com>
2024-11-26 20:10:07 -0300

    att botão delete

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const { Pool } = require('pg')
 
 const app = express()
+
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }))
-//teste
 
 app.use(express.json())
 
@@ -21,7 +21,7 @@ const pool = new Pool({
 })
 
 app.get('/rick/search/:query', async (req, res) => {
-    const { query } = req.params;
+    const { query } = req.params
 
     try {
         const result = await pool.query(
@@ -32,10 +32,10 @@ app.get('/rick/search/:query', async (req, res) => {
         );
         res.json(result.rows)
     } catch (error) {
-        console.error('Erro ao buscar registros:', error)
+        console.error('Erro ao buscar registros:', error);
         res.status(500).json({ message: 'Erro ao buscar registros.' })
     }
-});
+})
 
 app.get('/rick/list', async (req, res) => {
     try {
@@ -48,9 +48,9 @@ app.get('/rick/list', async (req, res) => {
 })
 
 app.get('/rick/check-cpf/:cpf', async (req, res) => {
-    var { cpf } = req.params
+    const { cpf } = req.params;
     try {
-        var resultado = await pool.query('SELECT cpf FROM suneca WHERE cpf = $1', [cpf])
+        const resultado = await pool.query('SELECT cpf FROM suneca WHERE cpf = $1', [cpf])
         
         if (resultado.rows.length > 0) {
             res.status(200).json({ exists: true })
@@ -63,10 +63,8 @@ app.get('/rick/check-cpf/:cpf', async (req, res) => {
     }
 })
 
-
-
 app.post('/rick/add', async (req, res) => {
-    console.log('Dados recebidos:', req.body);
+    console.log('Dados recebidos:', req.body)
     const { nomeInput, idadeInput, alturaInput, pesoInput, cpfInput } = req.body
 
     try {
@@ -75,11 +73,28 @@ app.post('/rick/add', async (req, res) => {
         await pool.query(
             'INSERT INTO suneca (nome, idade, altura, peso, cpf, dataregistro) VALUES ($1, $2, $3, $4, $5, NOW())',
             [nomeInput, idadeInput, alturaTratada, pesoInput, cpfInput]
-        );
-        res.status(201).json({ message: 'Mocorongo adicionado com sucesso!' })
+        )
+        res.status(201).json({ message: 'Mocorongo adicionado com sucesso!' });
     } catch (error) {
-        console.error('Erro ao adicionar mocorongo:', error)
-        res.status(500).json({ message: 'Erro ao adicionar Mocorongo.' })
+        console.error('Erro ao adicionar mocorongo:', error);
+        res.status(500).json({ message: 'Erro ao adicionar Mocorongo.' });
+    }
+})
+
+app.delete('/rick/delete/:cpf', async (req, res) => {
+    const { cpf } = req.params
+
+    try {
+        const result = await pool.query('DELETE FROM suneca WHERE cpf = $1 RETURNING *', [cpf])
+
+        if (result.rowCount > 0) {
+            res.status(200).json({ message: 'Mocorongo deletado com sucesso!' })
+        } else {
+            res.status(404).json({ message: 'Mocorongo não encontrado.' })
+        }
+    } catch (error) {
+        console.error('Erro ao deletar registro:', error)
+        res.status(500).json({ message: 'Erro ao deletar mocorongo.' })
     }
 })
 
